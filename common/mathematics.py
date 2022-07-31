2c6b8552baf592ed730fe01754267a0418fdc410
adaramola omolewa <info@nextlambdaconsulting.com>
2022-07-31 16:36:28 -0400

    classes

---
+++ common/mathematics.py
@@ -0,0 +1,8 @@
+def multiply(x):
+    return x * 100
+
+
+def divide(x):
+    return x / 4
+
+
---
+++ main.py
@@ -0,0 +1,11 @@
+
+from common import mathematics as mth
+from common import mod
+
+import math
+import pandas
+
+
+
+
+print(mth.multiply(9))
